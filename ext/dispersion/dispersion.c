1e3ab604c161958f98a3b1ae5db83f194d316328
John Owen <johnowenatala@gmail.com>
2017-04-10 19:37:07 -0300

    Added mean native method

--- ext/dispersion/dispersion.c
+++ ext/dispersion/dispersion.c
@@ -8,19 +8,29 @@ void Init_dispersion() {
   rb_define_method(DispersionModule, "stdevp", rb_population_standard_deviation, 0);
   rb_define_method(DispersionModule, "var", rb_sample_variance, 0);
   rb_define_method(DispersionModule, "varp", rb_population_variance, 0);
+  rb_define_method(DispersionModule, "mean", rb_mean, 0);
 }
 
-double calculate_total_distance_from_mean(VALUE array, unsigned long array_length){
+double calculate_mean(VALUE array, unsigned long array_length){
   unsigned long i;
   double total = 0;
   double mean = 0;
-  double total_distance_from_mean = 0;
 
   for(i = 0; i < array_length; i++){
     total += rb_num2dbl(rb_ary_entry(array, i));
   }
 
   mean = total / array_length;
+
+  return mean;
+}
+
+double calculate_total_distance_from_mean(VALUE array, unsigned long array_length){
+  unsigned long i;
+  double mean = 0;
+  double total_distance_from_mean = 0;
+
+  mean = calculate_mean(array, array_length);
 
   for(i = 0; i < array_length; i++){
     total_distance_from_mean += pow((rb_num2dbl(rb_ary_entry(array, i)) - mean), 2);
@@ -84,3 +94,18 @@ VALUE rb_population_variance(VALUE self) {
 
   return DBL2NUM(calculate_total_distance_from_mean(self, array_length) / array_length);
 }
+
+VALUE rb_mean(VALUE self) {
+  unsigned int array_length;
+
+  Check_Type(self, T_ARRAY);
+
+  array_length = rb_long2int(RARRAY_LEN(self));
+
+  if (array_length <= 0) {
+    rb_raise(rb_eRangeError, "array must have at least one element");
+  }
+
+  return DBL2NUM(calculate_mean(self, array_length));
+}
+
